653ff694a0fb2d87dd854b007959c7b14ec23daa
Anttoni Maula <manttoni92@gmail.com>
2025-01-26 21:55:26 +0200

    test.c

--- tester/test.c
+++ tester/test.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include "../includes/object.h"
-#include <math.h>
+#include <stdlib.h>
 
 int main(int ac, char **av)
 {
-	t_object *object = parse_object(av[1]);
-
 	if (ac != 2)
 		return 2;
+
+	t_object *object = parse_object(av[1]);
 
 	if (object == NULL)
 		return 1;
