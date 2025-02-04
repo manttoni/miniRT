1667229e182159b7b9da55c27751305f290120d3
¨Nikolai <nzharkev@student.hive.fi>
2025-02-04 13:27:03 +0200

    Working lights version

--- sources/color.c
+++ sources/color.c
@@ -31,19 +31,6 @@ void	base_pixel(uint8_t *buffer, int colour, int alpha)
 
 void	color_pixel(mlx_image_t *image, uint32_t pixel_color, int x, int y)
 {
-	// int	pixel_index;
-
-	// pixel_index = y * X + x;
-	// ((uint32_t *)(image->pixels))[pixel_index] = pixel_color;
-	// int	dot;
-	// int	alpha;
-
-	// alpha = 0xFF;
-	// if (x > X || y > Y || x < 0 || y < 0)
-	// 	return ;
-	// dot = ((int)round(y) * X * 4) + ((int)round(x) * 4);
-	// base_pixel(&image->pixels[dot], pixel_color, alpha);
-
 	if (x >= 0 && x < X && y >= 0 && y < Y)
 		mlx_put_pixel(image, x, y, pixel_color);
 }
